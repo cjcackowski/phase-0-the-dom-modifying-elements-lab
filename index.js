8f06037de179fee3edff188ab53cfc8a68c684e3
cjcackowski <ccackowski@gmail.com>
2024-02-26 23:54:53 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the 'main#main' node
+document.querySelector('main#main').remove();
+
+// Create <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new header to 'victory'
+newHeader.id = 'victory';
+
+// Set the innerHTML of the new header with the desired text
+newHeader.innerHTML = "Cecylia is the champion"; 
+
+// Append the new header to the body
+document.body.appendChild(newHeader);
